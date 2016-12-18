e54d97e9a175348a631c887aa3c0bd04bcb06e94
Bane-D <baned.ben@gmail.com>
2016-12-18 03:13:53 +0100

    Adding new auth token demos

---
+++ demos/create_authtoken.js
@@ -0,0 +1,19 @@
+ZiggeoSdk = require("../index.js");
+
+ZiggeoSdk.init(process.argv[2], process.argv[3], process.argv[4]);
+
+var token = process.argv[5];
+
+// You can send the data you wish to use in the token as parameter, or create it based on the parameter sent in, or simply hardcode the value into the code, that is all up to you. For simplicity we are hardcoding a value.
+
+//var data = process.argv[6];
+
+const data = {
+    session_limit: 200
+};
+
+ZiggeoSdk.Authtokens.create(data, {
+    success: function (result) {
+        console.log(result);
+    }
+});
