e03fb091aca17fd4c9702d8baf8b22483c460db0
triyono, ibnu <ibnu.triyono@gmail.com>
2017-03-25 10:50:57 +0700

    successfully delete all videos, and showing the state of deletion when the script is running.

--- demos/delete_all_videos.js
+++ demos/delete_all_videos.js
@@ -1,23 +1,46 @@
 ZiggeoSdk = require("../index.js");
 
 ZiggeoSdk.init(process.argv[2], process.argv[3]);
-
-ZiggeoSdk.Videos.index({}, {
-	success: function (index) {
-		var video_counter = 0;
-		console.log ('Starting to delete '+index.length+' videos.');
-		for (var i = 0; i < index.length; ++i) {
-			var video = index[i];
-			ZiggeoSdk.Videos.destroy(video.token,{
-				success: function (result) {
-					// to show progress on the console until it's done.
-					video_counter += 1;
-					console.log('Deleted video '+video_counter+' of '+index.length+' videos.');
-					if (video_counter == index.length) {
-						console.log ('Deletion of all videos is completed.');
-					}
+var video_to_delete = [];
+var video_to_skip = 0;
+console.log('Start fetching video tokens ');
+indexRemoveVideo(0);
+function indexRemoveVideo (skip) {
+	ZiggeoSdk.Videos.index({limit:100, skip: skip}, {
+		success: function (index) {
+			var video_counter = 0;
+			// console.log(data);
+			if (index.length > 0) {
+				for (var i = 0; i < index.length; ++i) {
+					video_to_delete.push(index[i].token);
 				}
-			});
+				video_to_skip += index.length;
+				indexRemoveVideo(video_to_skip);
+			}else{
+				console.log("Video tokens fetched : "+video_to_delete.length);
+				deleteVideo(video_to_delete);
+			}
+		},
+		failure: function (args, error) {
+			console.log("failed: " + error);
+			return false;
 		}
+	});
+}
+
+function deleteVideo (token_list) {
+	console.log('Start deleting '+token_list.length+' videos');
+	var video_counter = 0;
+	for (var i = 0; i < token_list.length; i++) {
+		ZiggeoSdk.Videos.destroy(token_list[i],{
+			success: function (result) {
+				// to show progress on the console until it's done.
+				video_counter += 1;
+				console.log('Deleted video '+video_counter+' of '+token_list.length+' videos');
+				if (video_counter == token_list.length) {
+					console.log ('Deletion of all videos is completed.');
+				}
+			}
+		});
 	}
-});
+}
