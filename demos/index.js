cf36aaa0ef3f41887619980fd45f60a5e0d93e8f
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-07-06 20:04:27 +0700

    adding index function sample

---
+++ demos/index.js
@@ -0,0 +1,21 @@
+ZiggeoSdk = require("../index.js");
+
+ZiggeoSdk.init(process.argv[2], process.argv[3]);
+
+console.log('Start fetching video tokens ');
+
+// 'limit' will limit how much index operation will fetch the videos. default 50 max 100
+ZiggeoSdk.Videos.index({limit:100}, {
+	success: function (index) {
+		var video_counter = 0;
+		if (index.length > 0) {
+			for (var i = 0; i < index.length; ++i) { // iterate fetched result
+				console.log(index[i]);
+			}
+		}
+	},
+	failure: function (args, error) {
+		console.log("failed: " + error);
+		return false;
+	}
+});
