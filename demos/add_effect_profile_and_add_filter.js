77e263e3533b8ddd6af4b6882363bf877a0ea112
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-07-20 16:17:07 +0700

    add effect profile and filter while doing so

---
+++ demos/add_effect_profile_and_add_filter.js
@@ -0,0 +1,22 @@
+ZiggeoSdk = require("../index.js");
+
+var app_token = process.argv[2];
+var private_key = process.argv[3];
+var effect_profile_title = process.argv[4];
+var effect_profile_key = process.argv[5];
+var filter_effect = process.argv[6]; // filtes are gray, cartoon, lucis, edge, chill, charcoal, sketch
+
+ZiggeoSdk.init(app_token, private_key);
+var args = {
+	title: effect_profile_title,
+	key: effect_profile_key
+};
+ZiggeoSdk.EffectProfiles.create(args ,function(data){
+	var profile_token = data.token;
+	var filterArgs = {
+		filter: filter_effect
+	};
+	ZiggeoSdk.EffectProfileProcess.create_filter_process(profile_token, filterArgs, function(data){
+		console.log(data);
+	});
+});
