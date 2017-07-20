3d80b69575c38b961272478520d1f8813e18cccd
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-07-20 15:46:56 +0700

    add effect profile script

---
+++ demos/add_effect_profile.js
@@ -0,0 +1,15 @@
+ZiggeoSdk = require("../index.js");
+
+var app_token = process.argv[2];
+var private_key = process.argv[3];
+var effect_profile_title = process.argv[4];
+var effect_profile_key = process.argv[5];
+
+ZiggeoSdk.init(app_token, private_key);
+var args = {
+	title: effect_profile_title,
+	key: effect_profile_key
+};
+ZiggeoSdk.EffectProfiles.create(args ,function(data){
+	console.log(data);
+});
