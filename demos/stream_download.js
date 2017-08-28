05b54ab0bba9b732428cddb56b02ea70ccd7a52f
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-08-28 20:01:23 +0700

    add script to download stream

---
+++ demos/stream_download.js
@@ -0,0 +1,21 @@
+ZiggeoSdk = require("../index.js");
+var fs = require('fs');
+var app_token = process.argv[2];
+var private_key = process.argv[3];
+var video_token = process.argv[4];
+var stream_token = process.argv[5];
+
+ZiggeoSdk.init(app_token, private_key);
+
+// you can find example of grants JSON at https://ziggeo.com/docs/api/authorization-tokens/examples
+ZiggeoSdk.Streams.get (video_token, stream_token, {
+    success: function (stream) {
+    	console.log(stream);
+        ZiggeoSdk.Streams.download_video (video_token, stream_token, function(data){
+	    	console.log('download');
+	        fs.writeFile(video_token+'_'+stream_token+'.'+stream.video_type, data, function(err){
+	            console.log('downloaded\n');
+	        });
+	    });
+    }
+});
