19654da0ad07fd7c67dbaa43a02cdcee21714270
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-08-28 11:04:55 +0700

    add script to index all stream for certain video

---
+++ demos/stream_index.js
@@ -0,0 +1,22 @@
+ZiggeoSdk = require("../index.js");
+
+var api_token = process.argv[2];
+var private_key = process.argv[3];
+var video_token = process.argv[4];
+ZiggeoSdk.init(api_token, private_key);
+ 
+// 'limit' will limit how much index operation will fetch the videos. default 50 max 100
+ZiggeoSdk.Streams.index(video_token, {state:'ready'}, {
+	success: function (index) {
+		var video_counter = 0;
+		if (index.length > 0) {
+			for (var i = 0; i < index.length; ++i) { // iterate fetched result
+				console.log(index[i]);
+			}
+		}
+	},
+	failure: function (args, error) {
+		console.log("failed: " + error);
+		return false;
+	}
+});
