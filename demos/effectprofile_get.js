c3107328998504cd567c5a3a55462ebbfd9224db
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-09-04 11:30:38 +0700

    change effectprofile get to do it's proper function (previously it's indexing)

--- demos/effectprofile_get.js
+++ demos/effectprofile_get.js
@@ -1,12 +1,12 @@
 ZiggeoSdk = require("../index.js");
 var app_token = process.argv[2];
 var app_private = process.argv[3];
-
+var effectprofile_token = process.argv[5];
 
 ZiggeoSdk.init (app_token, app_private);
 var search_arguments = {
 	limit: 50
 }
-ZiggeoSdk.EffectProfiles.index (search_arguments, function(effects){
+ZiggeoSdk.EffectProfiles.get (search_arguments, function(effects){
 	console.log(effects);
 });
