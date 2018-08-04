d8e9fdf1247e2aafbd0fbef4d4a46e6766860db9
Ibnu triyono <ibnu.triyono@gmail.com>
2018-08-04 14:54:39 +0700

    add demo for bulk updating expiration days

---
+++ demos/video_expiration_days_bulk.js
@@ -0,0 +1,21 @@
+/*
+	Usage: node video_expiration_days_bulk.js APP_TOKEN PRIVATE_KEY "VIDEO1,VIDEO2,VIDEO3"  EXP_DAY_INT
+*/
+
+var fs = require('fs');
+var app_token = process.argv[2];
+var private_key = process.argv[3];
+var video_tokens = process.argv[4];
+var expiration_days = process.argv[5];
+
+var Ziggeo = require("../index.js");
+var ZiggeoSdk = new Ziggeo(app_token, private_key);
+
+ZiggeoSdk.Videos.update_bulk({tokens_or_keys:video_tokens,expiration_days: expiration_days},{
+	success: function(data){
+		console.log(data);
+	},
+	failure: function(error){
+		console.log(error);
+	}
+});
